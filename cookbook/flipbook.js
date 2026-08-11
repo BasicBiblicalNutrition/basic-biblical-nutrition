c900a95d46cc4a12cca3f1a057b984776149e561
BasicBiblicalNutrition <threedfx1@gmail.com>
2026-08-11 11:20:36 -0500

    added a hamburger

---
+++ cookbook/flipbook.js
@@ -0,0 +1,17 @@
+/*
+=====================================================
+FlipBook
+JavaScript Ver 1
+
+SECTION INDEX
+
+01 - Navigation Underline
+02 - Mobile Menu
+03 - Page Initialization
+
+=====================================================
+*/
+
+document.getElementById("home").addEventListener("click", () => {
+  window.location.href = "../index.html";
+});
