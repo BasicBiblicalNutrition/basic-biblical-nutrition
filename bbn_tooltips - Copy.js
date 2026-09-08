f587b356edd5e12b1d0840fef397655f07d04f84
BasicBiblicalNutrition <threedfx1@gmail.com>
2026-09-07 22:07:07 -0500

    ee

--- bbn_tooltips - Copy.js
+++ bbn_tooltips - Copy.js
@@ -103,7 +103,7 @@ function initializeTooltips() {
             } else {
                 // phone / far left
                 tooltip.style.left =
-                    (window.scrollX + r.left - tooltip.offsetWidth + 30) + "px";
+                    (window.scrollX + r.left - tooltip.offsetWidth ) + "px";
             }
 
            tooltip.style.top =
